b81fd0d658b9cade10ab3341044847f0077a4d7d
Bailey Li <baileyli@example.com>
2025-10-20 22:22:48 +0800

    配置Cloudflare部署

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   images: {
     domains: ['usebear.ai'],
     formats: ['image/avif', 'image/webp'],
+    unoptimized: true,
   },
+  output: 'export',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
